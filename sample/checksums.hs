0ed3e52422c289144b0e2a6d9ddf17c90eb47c57
bartavelle <bartavelle@gmail.com>
2017-11-22 13:41:35 +0100

    Make sample runnable

--- sample/checksums.hs
+++ sample/checksums.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE PackageImports #-}
+module Main where
+
 import qualified Crypto.Hash.Conduit as CH
 import qualified Data.Conduit.Tar    as CT
 
 import Conduit
-import Crypto.Hash (Digest, SHA256)
+import "cryptohash" Crypto.Hash (Digest, SHA256)
 import Control.Monad (when)
 import Data.Conduit.Zlib (ungzip)
 import Data.ByteString (ByteString)
+import System.Environment
 
 filedigests :: FilePath -> IO ()
 filedigests fp = runConduitRes (  sourceFileBS fp          -- read the raw file
@@ -19,3 +23,6 @@ filedigests fp = runConduitRes (  sourceFileBS fp          -- read the raw file
         hashentry hdr = when (CT.headerFileType hdr == CT.FTNormal) $ do
             hash <- CH.sinkHash
             yield (CT.headerFilePath hdr, hash)
+
+main :: IO ()
+main = getArgs >>= mapM_ filedigests
