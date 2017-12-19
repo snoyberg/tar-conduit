76129dd459de628d10565513b16d16647f1782a4
Alexey Kuleshevich <alexey@fpcomplete.com>
2017-12-19 21:44:10 +0300

    Fix for environments without terminal. Updated ChangeLog

--- src/Data/Conduit/Tar/Unix.hs
+++ src/Data/Conduit/Tar/Unix.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.Conduit.Tar.Unix
     ( getFileInfo
     , restoreFile
     ) where
 
 import           Conduit
+import           Control.Exception
 import           Control.Monad                 (when)
 import           Data.Bits
 import qualified Data.ByteString.Char8         as S8
@@ -19,8 +21,11 @@ getFileInfo fp = do
     fs <- Posix.getSymbolicLinkStatus fp
     let uid = Posix.fileOwner fs
         gid = Posix.fileGroup fs
-    uEntry <- Posix.getUserEntryForID uid
-    gEntry <- Posix.getGroupEntryForID gid
+    -- Allow for username/group retrieval failure, especially useful for non-tty environment.
+    -- Workaround for: https://ghc.haskell.org/trac/ghc/ticket/1487
+    -- Moreover, names are non-critical as they are not used during unarchival process
+    euEntry :: Either IOException Posix.UserEntry <- try $ Posix.getUserEntryForID uid
+    egEntry :: Either IOException Posix.GroupEntry <- try $ Posix.getGroupEntryForID gid
     (fType, fSize) <-
         case () of
             () | Posix.isRegularFile fs     -> return (FTNormal, Posix.fileSize fs)
@@ -35,9 +40,9 @@ getFileInfo fp = do
     return FileInfo
         { filePath      = fp
         , fileUserId    = uid
-        , fileUserName  = S8.pack $ Posix.userName uEntry
+        , fileUserName  = either (const "") (S8.pack . Posix.userName) euEntry
         , fileGroupId   = gid
-        , fileGroupName = S8.pack $ Posix.groupName gEntry
+        , fileGroupName = either (const "") (S8.pack . Posix.groupName) egEntry
         , fileMode      = Posix.fileMode fs .&. 0o7777
         , fileSize      = fSize
         , fileType      = fType
